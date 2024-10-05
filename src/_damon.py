a207b9b41569032253d4841315da8479f2251e5d
SeongJae Park <sj@kernel.org>
2024-10-05 12:51:32 -0700

    _damon: Implement ProcStat.__str__() for debugging purpose
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_damon.py
+++ src/_damon.py
@@ -948,6 +948,24 @@ class ProcStat:
         with open(file_path, 'r') as f:
             self.fields = f.read().split()
 
+    def __str__(self):
+        # from Documentation/filesystesm/proc.rst of Linux source tree
+        field_names = ['pid', 'tcomm', 'state', 'ppid', 'pgrp', 'sid',
+                       'tty_nr', 'tty_pgrp', 'flags', 'min_flt', 'cmin_flt',
+                       'maj_flt', 'cmaj_flt', 'utime', 'stime', 'cutime',
+                       'cstime', 'priority', 'nice', 'num_threads',
+                       'it_real_value', 'start_time', 'vsize', 'rss', 'rsslim',
+                       'start_code', 'end_code', 'start_stack', 'esp', 'eip',
+                       'pending', 'blocked', 'sigign', 'sigcatch', '0', '0',
+                       '0', 'exit_signal', 'task_cpu', 'rt_priority', 'policy',
+                       'blkio_ticks', 'gtime', 'cgtime', 'start_data',
+                       'end_data', 'start_brk', 'arg_start', 'arg_end',
+                       'env_start', 'env_end', 'exit_code']
+        lines = []
+        for idx, field_name in enumerate(field_names):
+            lines.append('%s: %s' % (field_name, self.fields[idx]))
+        return '\n'.join(lines)
+
 class Kdamond:
     state = None
     pid = None
