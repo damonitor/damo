6447d7994562b9b49538893590ba8163502c524e
SeongJae Park <sj@kernel.org>
2024-12-18 19:57:35 -0800

    _damon: add sz_ops_filter_passed on the features support list
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_damon.py
+++ src/_damon.py
@@ -1055,6 +1055,7 @@ features = ['record',       # was in DAMON patchset, but not merged in mainline
             'schemes_quota_goal_some_psi',  # merged in v6.9-rc1
             'schemes_filters_young',    # merged in v6.10-rc1
             'schemes_migrate',          # merged in v6.11-rc1
+            'sz_ops_filter_passed',     # developing
             ]
 
 _damon_fs = None
