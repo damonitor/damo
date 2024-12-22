96f65c02d26ecd86591b4b06e26be3ae4075e38c
SeongJae Park <sj@kernel.org>
2024-12-22 10:39:11 -0800

    damo_report_access: add <filter passed bytes> region if --tried_regions_of is given
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_access.py
+++ src/damo_report_access.py
@@ -945,6 +945,9 @@ def set_formats(args):
             args.format_snapshot_tail = ('%s\n<region box description>' %
                     args.format_record_tail)
 
+    if args.tried_regions_of and args.format_region == default_region_format:
+        args.format_region += ' filters-passed <filters passed bytes>'
+
     return RecordsVisualizationFormat.from_args(args)
 
 def handle_ls_keywords(args):
