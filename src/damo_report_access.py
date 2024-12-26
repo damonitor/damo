6307da118b11a31893609cdae0300eeeff30241c
SeongJae Park <sj@kernel.org>
2024-12-26 11:44:00 -0800

    damo_report_access: display filter on snapshot head
    It is repetitive to show the filters passed type for each region.
    Display it at snapshot head once.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_access.py
+++ src/damo_report_access.py
@@ -105,6 +105,10 @@ snapshot_formatters = [
                   lambda snapshot, record, fmt:
                   heatmap_str(snapshot, record, fmt),
                   'heatmap of the snapshot'),
+        Formatter(
+            '<filters passed type>',
+            lambda snapshot, record, fmt: fmt.region_filters_pass_type,
+            'type of <filters passed bytes> memory'),
         ]
 
 region_formatters = [
@@ -150,10 +154,6 @@ region_formatters = [
             _damo_fmt_str.format_sz(region.sz_filter_passed, fmt.raw_number)
             if region.sz_filter_passed is not None else 'N/A',
             'bytes of the region that passed DAMOS filters'),
-        Formatter(
-            '<filters passed type>',
-            lambda index, region, fmt: fmt.region_filters_pass_type,
-            'type of <filters passed bytes> memory'),
         Formatter(
             '<box>',
             lambda index, region, fmt:
@@ -918,6 +918,8 @@ class RecordsVisualizationFormat:
                 self.format_snapshot_head = default_snapshot_head_format
             else:
                 self.format_snapshot_head = 'heatmap: <heatmap>'
+        if '<filters passed bytes>' in self.format_region:
+            self.format_snapshot_head += '\n# filters: <filters passed type>'
 
 
 def set_formats(args):
@@ -958,8 +960,7 @@ def set_formats(args):
 
     if args.format_region == default_region_format:
         if args.tried_regions_of or args.damos_filter:
-            args.format_region += ' %s %s' % (
-                    '<filters passed type>', '<filters passed bytes>')
+            args.format_region += ' filters passed <filters passed bytes>'
 
     return RecordsVisualizationFormat.from_args(args)
 
