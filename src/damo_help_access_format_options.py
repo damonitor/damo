5d0f97cb5daf853b41a9fb2a4dfebc18f2dc1739
SeongJae Park <sj@kernel.org>
2025-04-13 11:34:11 -0700

    damo_help_access_format_options: provide format keywords help
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_help_access_format_options.py
+++ src/damo_help_access_format_options.py
@@ -5,6 +5,19 @@ import argparse
 import damo_report_access
 
 def main(args):
+    if args.subtopic == 'record_format_keywords':
+        print('\n\n'.join(
+            ['%s' % f for f in damo_report_access.record_formatters]))
+        return
+    if args.subtopic == 'snapshot_format_keywords':
+        print('\n\n'.join(
+            ['%s' % f for f in damo_report_access.snapshot_formatters]))
+        return
+    if args.subtopic == 'region_format_keywords':
+        print('\n\n'.join(
+            ['%s' % f for f in damo_report_access.region_formatters]))
+        return
+
     print('command line options for monitoring results visualization options')
     parser = argparse.ArgumentParser(add_help=False)
     damo_report_access.add_fmt_args(parser)
@@ -19,4 +32,10 @@ def main(args):
 
 def set_argparser(parser):
     parser.description = 'help for DAMON monitoring results visualization options'
+    parser.add_argument(
+            'subtopic', nargs='?',
+            default='options',
+            choices=['options', 'record_format_keywords',
+                     'snapshot_format_keywords', 'region_format_keywords'],
+            help='sub-topic to get help')
     return parser
