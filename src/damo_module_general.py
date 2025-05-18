6f358bdbd2dbce5a1424f9288d7727effdfc82a7
SeongJae Park <sj@kernel.org>
2025-05-18 11:13:14 -0700

    damo_module_general: read all parameters if no parameter name is given
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_module_general.py
+++ src/damo_module_general.py
@@ -10,8 +10,13 @@ def main(args):
     module_name = args.module_name
     parm_dir = '/sys/module/damon_%s/parameters' % module_name
     if args.action == 'read':
-        with open(os.path.join(parm_dir, args.parameter), 'r') as f:
-            print(f.read().strip())
+        if args.parameter is not None:
+            with open(os.path.join(parm_dir, args.parameter), 'r') as f:
+                print(f.read().strip())
+        else:
+            for parm in os.listdir(parm_dir):
+                with open(os.path.join(parm_dir, parm), 'r') as f:
+                    print('%s: %s' % (parm, f.read().strip()))
     elif args.action == 'write':
         parm_name, parm_val = args.parameter_value
         with open(os.path.join(parm_dir, parm_name), 'w') as f:
@@ -24,7 +29,7 @@ def set_argparser(parser):
 
     parser_read = subparsers.add_parser('read', help='read parameters')
     parser_read.add_argument(
-            'parameter', metavar='<parameter name>',
+            'parameter', metavar='<parameter name>', nargs='?',
             help='parameter to read.')
 
     parser_write = subparsers.add_parser('write', help='write parameters')
