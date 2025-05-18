1f4364fa16e53d6d0cfaa7965a7fff261ee52355
SeongJae Park <sj@kernel.org>
2025-05-18 10:40:53 -0700

    damo_modules: support all damon modules
    Support all built DAMON modules, not only DAMON_RECLAIM and
    DAMON_LRU_SORT.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_modules.py
+++ src/damo_modules.py
@@ -1,7 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import os
+
 import _damo_subcmds
 import damo_lru_sort
+import damo_module_general
 import damo_reclaim
 
 subcmds = [
@@ -16,12 +19,25 @@ def main(args):
         if subcmd.name == args.module_name:
             subcmd.execute(args)
 
+def set_subcmds(subcmds):
+    for module in os.listdir('/sys/module'):
+        if not module.startswith('damon_'):
+            continue
+        module_name = module[len('damon_'):]
+        if module_name in ['reclaim', 'lru_sort']:
+            continue
+        subcmds.append(_damo_subcmds.DamoSubCmd(
+            name=module_name, module=damo_module_general,
+            msg='Control DAMON_%s' % module_name.upper()))
+
 def set_argparser(parser):
     subparsers = parser.add_subparsers(
             title='DAMON module name', dest='module_name',
             metavar='<module name>',
             help='the name of DAMON module to control')
     subparsers.required = True
+
+    set_subcmds(subcmds)
 
     for subcmd in subcmds:
         subcmd.add_parser(subparsers)
