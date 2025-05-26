91eefff58eac79eead504c7e69bcdb049976ad19
SeongJae Park <sj@kernel.org>
2025-05-26 10:13:16 -0700

    damo_start: introduce --include_child_tasks option
    For virtual address space DAMON usages, it can be useful to
    automatically monitor monitoring target's child tasks and update DAMON
    monitoring targets to include those.  Only 'damo record' has similar
    purpose option, namely '--include_child_tasks'.  Add a similar option to
    'damo start'.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_start.py
+++ src/damo_start.py
@@ -5,9 +5,16 @@ Start DAMON with given parameters.
 """
 
 import os
+import signal
+import time
 
+import _damo_records
 import _damon
 import _damon_args
+
+def sighandler(signum, frame):
+    print('\nsingal %s received' % signum)
+    exit(0)
 
 def main(args):
     _damon.ensure_root_and_initialized(args)
@@ -35,7 +42,18 @@ def main(args):
         print('could not turn on damon (%s)' % err)
         exit(1)
 
+    if args.include_child_tasks is True:
+        signal.signal(signal.SIGINT, sighandler)
+        signal.signal(signal.SIGTERM, sighandler)
+        print('Continue monitoring child tasks and updating DAMON targets')
+        print('Press Ctrl+C to stop')
+        while True:
+            _damo_records.add_childs_target(kdamonds)
+            time.sleep(3)
+
 def set_argparser(parser):
     _damon_args.set_argparser(parser, add_record_options=False, min_help=True)
+    parser.add_argument('--include_child_tasks', action='store_true',
+                        help='add child tasks as monitoring target')
     parser.description = 'Start DAMON with specified parameters'
     return parser
