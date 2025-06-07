f678e1056d5927b722086ffb3b4e82b15dde46d2
SeongJae Park <sj@kernel.org>
2025-06-07 12:58:38 -0700

    damo_report_heatmap: fix wrong heat time accounting
    The logic is not accounting region's age.  Account it.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_heatmap.py
+++ src/damo_report_heatmap.py
@@ -80,9 +80,13 @@ class HeatMap:
     def pixel_idx_of_addr(self, addr):
         return int((addr - self.addr_start) / self.addr_unit)
 
-    def add_pixel_heat(self, pixels_idx, pixel_idx, region, snapshot):
+    def add_pixel_heat(self, pixels_idx, pixel_idx, region, snapshot, aggr_ns):
         pixel = self.pixels[pixels_idx][pixel_idx]
-        account_time_start = max(snapshot.start_time, pixel.time)
+        observe_start_time = snapshot.start_time
+        if aggr_ns is not None:
+            observe_start_time -= region.age.aggr_intervals * aggr_ns
+
+        account_time_start = max(observe_start_time, pixel.time)
         account_time_end = min(snapshot.end_time, pixel.time + self.time_unit)
         account_time = account_time_end - account_time_start
 
@@ -126,7 +130,7 @@ class HeatMap:
                     if pixel_idx < 0 or self.addr_resol <= pixel_idx:
                         continue
                     self.add_pixel_heat(
-                            pixels_idx, pixel_idx, region, snapshot)
+                            pixels_idx, pixel_idx, region, snapshot, aggr_ns)
 
     def highest_lowest_heats(self):
         highest = None
