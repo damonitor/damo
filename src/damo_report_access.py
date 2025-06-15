4f916bb810ca4131bbe0a070149ba082f363ae06
SeongJae Park <sj@kernel.org>
2025-06-15 10:56:03 -0700

    damo_report_access: stop warning allow filters
    6.15 is released.  Stop warning allow filters at the end of the list.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_access.py
+++ src/damo_report_access.py
@@ -322,8 +322,6 @@ def filters_pass_type_of(record):
     if len(ops_filters) == 0:
         return 'n/a'
     text = ', '.join(['%s' % f for f in ops_filters])
-    if ops_filters[-1].allow is True:
-        text += '\n# WARN: Allow filters at the end of the list means nothing on <6.15'
     return text
 
 def get_linearscale_hist_ranges(minv, maxv, nr_ranges, cumulate):
