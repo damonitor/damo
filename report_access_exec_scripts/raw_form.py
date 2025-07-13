e7a5b1e2d93e3b767d9ca58017cd66d9fd6a5952
SeongJae Park <sj@kernel.org>
2025-07-13 10:30:53 -0700

    report_access_exec_scripts/raw_form: use samples and aggr_intervals units
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- report_access_exec_scripts/raw_form.py
+++ report_access_exec_scripts/raw_form.py
@@ -3,9 +3,12 @@
 def main(records, cmdline_fields):
     for ridx, record in enumerate(records):
         print('%d-th record' % ridx)
+        print('intervals: %d %d' %
+              (record.intervals.sample, record.intervals.aggr))
         for sidx, snapshot in enumerate(record.snapshots):
             print('%d-th snapshot' % sidx)
+            print('snapshot sample interval: %d' % snapshot.sample_interval_us)
             for region in snapshot.regions:
                 print('%s-%s (%s): %s %s' % (
                     region.start, region.end, region.size(),
-                    region.nr_accesses.percent, region.age.usec))
+                    region.nr_accesses.sample, region.age.aggr_intervals))
