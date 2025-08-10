ad1a2e1a4d226a7c32d9b17ea2f403862d788b6c
SeongJae Park <sj@kernel.org>
2025-08-10 10:25:27 -0700

    report_access_exec_scripts: add a script for per idle time memory size
    This can be useful for seeing the long cold memory tail.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ report_access_exec_scripts/mem_sz_per_idle_time.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-2.0
+
+def idle_time(region):
+    if region.nr_accesses.samples > 0:
+        return 0
+    return region.age.usec
+
+def pr_idle_time_mem_sz(regions):
+    regions = sorted(regions, key=lambda r: idle_time(r))
+    total_sz = sum([r.size() for r in regions])
+    max_idle_time = idle_time(regions[-1])
+    min_idle_time = idle_time(regions[0])
+    idle_time_interval = (max_idle_time - min_idle_time) / 100
+    next_idle_time_to_pr = min_idle_time + idle_time_interval
+    sz = 0
+    for r in regions:
+        if idle_time(r) > next_idle_time_to_pr:
+            print('%f\t%f' % (next_idle_time_to_pr, sz / total_sz * 100))
+            next_idle_time_to_pr += idle_time_interval
+            sz = r.size()
+            continue
+        sz += r.size()
+
+def main(records, cmdline_fields):
+    for ridx, record in enumerate(records):
+        for sidx, snapshot in enumerate(record.snapshots):
+            print('# %d-th record, %d-th snapshot' % (ridx, sidx))
+            print('# <idle time (seconds)> <memory size in percentage>')
+            for region in snapshot.regions:
+                region.age.add_unset_unit(record.intervals)
+            pr_idle_time_mem_sz(snapshot.regions)
