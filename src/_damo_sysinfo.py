8258eede1a1bf88f07237a0833b8fdbf747a33ed
SeongJae Park <sj@kernel.org>
2025-11-01 13:38:24 -0700

    _damo_sysinfo: define data structures
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_damo_sysinfo.py
+++ src/_damo_sysinfo.py
@@ -4,3 +4,19 @@
 Contains code for managing system information including kernel version and
 DAMON features enabled on the kernel.
 '''
+
+class DamonFeature:
+    name = None
+    upstream_status = None
+    comments = None
+
+    def __init__(self, name, upstream_status, comments=''):
+        self.name = name
+        self.upstream_status = upstream_status
+        self.comments = comments
+
+class SystemInfo:
+    damo_version = None
+    kernel_version = None
+    avail_damon_debugfs_features = None
+    avail_damon_sysfs_features = None
