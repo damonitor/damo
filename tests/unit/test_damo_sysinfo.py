0ede45e099f69ab23640dd8f29d508c3796e6c41
SeongJae Park <sj@kernel.org>
2025-11-02 09:04:13 -0800

    tests/unit/test_damo_sysinfo: add a test for SystemInfo to/from kvpairs conversion
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/unit/test_damo_sysinfo.py
+++ tests/unit/test_damo_sysinfo.py
@@ -17,5 +17,20 @@ class TestDamoSysinfo(unittest.TestCase):
         f2 = _damo_sysinfo.DamonFeature.from_kvpairs(kvpairs)
         self.assertEqual(f, f2)
 
+    def test_systeminfo_kvpair_conversion(self):
+        features = []
+        for i in range(4):
+            features.append(_damo_sysinfo.DamonFeature(
+                name='foo %s' % i, upstream_status='bar %s' % i,
+                comments='baz %s' % i))
+        sinfo = _damo_sysinfo.SystemInfo(
+                damo_version='v3.1.1',
+                kernel_version='6.18.0-rc2-mm-new-damon+',
+                avail_damon_sysfs_features=[f for f in features[:2]],
+                avail_damon_debugfs_features=[f for f in features[2:]])
+        kvpairs = sinfo.to_kvpairs()
+        sinfo2 = _damo_sysinfo.SystemInfo.from_kvpairs(kvpairs)
+        self.assertEqual(sinfo, sinfo2)
+
 if __name__ == '__main__':
     unittest.main()
