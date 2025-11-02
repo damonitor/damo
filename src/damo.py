933bc69a9a6240bc7feb4b26054c8bfd3c8c05b8
SeongJae Park <sj@kernel.org>
2025-11-02 10:48:03 -0800

    damo: fix a typo: s/symlonk/symlink/
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo.py
+++ src/damo.py
@@ -7,7 +7,7 @@ import os
 damo_dir = os.path.dirname(os.path.abspath(__file__))
 os.sys.path.insert(0, damo_dir)
 
-# on some distros, symlonk damo's directory is added to the os.sys.path,
+# on some distros, symlink damo's directory is added to the os.sys.path,
 # instead of the damo.py's directory.  In the case, using damo from local repo
 # fails.  Check the case and modify os.sys.path, accordingly.
 if not os.path.isfile(os.path.join(damo_dir, 'damo_version.py')):
