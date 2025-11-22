40390617118aa35e906e3b8339e6894243be68fa
SeongJae Park <sj@kernel.org>
2025-11-22 10:53:56 -0800

    damo_version: implement a function for git-based version
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_version.py
+++ src/damo_version.py
@@ -1,9 +1,24 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import os
+import subprocess
+
+import _damo_subproc
+
 __version__ = '3.0.5'
 
 def get_release_version():
     return __version__
+
+def get_real_version():
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    damo_dir = os.path.dirname(src_dir)
+    if not os.path.isdir(os.path.join(damo_dir, '.git')):
+        return get_release_version()
+    if not _damo_subproc.avail_cmd('git'):
+        return get_release_version()
+    return subprocess.check_output(
+            ['git', '-C', damo_dir, 'describe']).decode().strip()
 
 def main(args):
     # todo: print 'git describe' if git repo version is being used.
