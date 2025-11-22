657953fa69e18e8cb962c16e4292d465e4dd9e9e
SeongJae Park <sj@kernel.org>
2025-11-22 10:42:06 -0800

    damo_version: implement a function for reading the version
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_version.py
+++ src/damo_version.py
@@ -2,6 +2,9 @@
 
 __version__ = '3.0.5'
 
+def get_release_version():
+    return __version__
+
 def main(args):
     # todo: print 'git describe' if git repo version is being used.
     print(__version__)
