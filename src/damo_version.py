cb6bb896ce95e9b8ab15d3e9ed57c568b925c82f
SeongJae Park <sj@kernel.org>
2025-11-23 13:09:30 -0800

    damo_version: use '--git-dir' insted of '-C' for git
    Otherwise, record test fails with dubious ownership issue.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_version.py
+++ src/damo_version.py
@@ -13,12 +13,13 @@ def get_release_version():
 def get_real_version():
     src_dir = os.path.dirname(os.path.abspath(__file__))
     damo_dir = os.path.dirname(src_dir)
-    if not os.path.isdir(os.path.join(damo_dir, '.git')):
+    git_dir = os.path.join(damo_dir, '.git')
+    if not os.path.isdir(git_dir):
         return get_release_version()
     if not _damo_subproc.avail_cmd('git'):
         return get_release_version()
     return subprocess.check_output(
-            ['git', '-C', damo_dir, 'describe']).decode().strip()
+            ['git', '--git-dir', git_dir, 'describe']).decode().strip()
 
 def main(args):
     print(get_release_version())
