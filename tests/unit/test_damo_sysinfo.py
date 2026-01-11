b091a0a42327592c249db53000f07140e9b6f5f1
SeongJae Park <sj@kernel.org>
2026-01-11 11:46:53 -0800

    tests/unit: setup SystemInfo.avail_damon_features
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/unit/test_damo_sysinfo.py
+++ tests/unit/test_damo_sysinfo.py
@@ -27,6 +27,7 @@ class TestDamoSysinfo(unittest.TestCase):
         sinfo = _damo_sysinfo.SystemInfo(
                 damo_version='v3.1.1',
                 kernel_version='6.18.0-rc2-mm-new-damon+',
+                avail_damon_features=[f for f in features],
                 avail_damon_sysfs_features=[f for f in features[:2]],
                 avail_damon_debugfs_features=[f for f in features[2:]],
                 avail_damon_trace_features=[])
