aef07c9c1df92239048d1ccdec24b8c06a5b5972
SeongJae Park <sj@kernel.org>
2026-04-05 09:17:54 -0700

    _damo_deprecation_notice: add alternative parameter to will_be_deprecated()
    It is common to use additional_notice for showing the alternative.  Let
    it simply recieves the alternative.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_damo_deprecation_notice.py
+++ src/_damo_deprecation_notice.py
@@ -2,17 +2,25 @@
 
 import sys
 
-def will_be_deprecated(feature, deadline, additional_notice=''):
-    sys.stderr.write('\n'.join([
-'',
-'WARNING: %s will be deprecated by %s.' % (feature, deadline),
-'    %s' % additional_notice,
-'    Please report your usecase to Github issues[1], sj@kernel.org,',
-'    damon@lists.linux.dev and/or linux-mm@kvack.org if you depend on those.',
-'',
-'    [1] https://github.com/damonitor/damo/issues',
-'',
-'']))
+def will_be_deprecated(feature, deadline, additional_notice='',
+                       alternative=None):
+    lines = [
+            '',
+            'WARNING: %s will be deprecated by %s.' % (feature, deadline)
+            ]
+    if alternative:
+        lines.append('    Use "%s" instead.' % alternative)
+    if additional_notice:
+        lines.append(additional_notice)
+    lines += [
+            '    Please report your usecase to Github issues[1], sj@kernel.org,',
+            '    damon@lists.linux.dev and/or linux-mm@kvack.org if you depend on those.',
+            '',
+            '    [1] https://github.com/damonitor/damo/issues',
+            '',
+            '',
+            ]
+    sys.stderr.write('\n'.join(lines))
 
 def deprecated(feature, deadline, do_exit=False, exit_code=1,
         additional_notice=''):
