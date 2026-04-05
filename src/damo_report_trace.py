da38c3bb9ac8ab78db0df07df92b75a6c698f834
SeongJae Park <sj@kernel.org>
2026-04-05 10:45:38 -0700

    damo_report_trace: support reporting for recorded tracepoints
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_trace.py
+++ src/damo_report_trace.py
@@ -21,7 +21,54 @@ def signalhandler(signum, frame):
 
 damon_trace_events = _damo_sysinfo.tracepoint_to_feature_name_map.keys()
 
+def report_recorded_trace(args):
+    trace_text_format = None
+    try:
+        trace_text = subprocess.check_output(
+                ['perf', 'script', '--force', '-i', args.input]).decode()
+        trace_text_format = 'perf'
+    except Exception as perf_e:
+        try:
+            trace_text = subprocess.check_output(
+                    ['trace-cmd', 'report', '-i', args.input]).decode()
+            trace_text_format = 'trace-cmd'
+        except Exception as trace_cmd_e:
+            print('cannot parse %s using neither perf (%s) nor trace-cmd (%s)'
+                  % (args.input, perf_e, trace_cmd_e))
+            return -1
+
+    if args.event is None:
+        print('--event is required')
+        exit(1)
+
+    events = []
+
+    if 'all' in args.event:
+        events = list(damon_trace_events)
+    else:
+        events = args.event
+    if args.no_event is not None:
+        for event in args.no_event:
+            events.remove(event)
+
+    for line in trace_text.split('\n'):
+        fields = line.split()
+        # perf script and trace-cmd report puts the event name on fifth field.
+        # perf:
+        #   kdamond.0  4452 [000] 82877.315633: damon:damon_aggregated: ...
+        # trace-cmd:
+        #   kdamond.0-264454 [007] ..... 92627.258073: damon_aggregated: ...
+        if len(fields) < 5:
+            continue
+        event = fields[4][:-1]
+        if not event in events:
+            continue
+        print(line)
+
 def main(args):
+    if args.input is not None:
+        return report_recorded_trace(args)
+
     global tracer_pipe
 
     _damon.ensure_root_permission()
@@ -78,3 +125,5 @@ def set_argparser(parser):
             help='events to skip tracing')
     parser.add_argument('--tracer', choices=['perf', 'trace-cmd'],
                         help='tracer command to use')
+    parser.add_argument('--input', '-i', metavar='<file>',
+                        help='trace record file')
