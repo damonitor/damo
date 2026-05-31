367020aa0c805d7ea4896f1c171aba4867f64ef0
SeongJae Park <sj@kernel.org>
2026-05-31 13:47:23 -0700

    tests/unit/test_damo_report_trace: add a test for parse_trace_line()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tests/unit/test_damo_report_trace.py
+++ tests/unit/test_damo_report_trace.py
@@ -34,5 +34,19 @@ class TestDamoReportTrace(unittest.TestCase):
             'target_id=0 nr_regions=11 4096-12288: 13 27 probe_hits=12 0f'.split()),
             '0 1/11 4.000000 KiB (8.000000 KiB) 13 27 18 15')
 
+    def test_parse_trace_line(self):
+        outputs = damo_report_trace.parse_trace_line(
+                '  1241.509 kdamond.0/102481 damon:damon_aggregated(target_id=0 nr_regions=11 8232701952-8371830784: 0 1485)', 'perf')
+        self.assertEqual(outputs, (
+            'kdamond.0/102481', '1241.509', 'damon:damon_aggregated',
+            'target_id=0 nr_regions=11 8232701952-8371830784: 0 1485'.split()))
+
+        outputs = damo_report_trace.parse_trace_line(
+                '           <...>-102481 [006] ..... 99141.640633: damon_aggregated:     target_id=0 nr_regions=12 4185931776-5007028224: 0 2022',
+                'trace-cmd')
+        self.assertEqual(outputs, (
+            '<...>-102481', '99141.640633', 'damon:damon_aggregated',
+            'target_id=0 nr_regions=12 4185931776-5007028224: 0 2022'.split()))
+
 if __name__ == '__main__':
     unittest.main()
