66098788b4ba7d6cdea55e7af65a24696a105444
SJ Park <sj@kernel.org>
2026-07-12 13:51:19 -0700

    damo_report_attrs: do not show access frequency
    Signed-off-by: SJ Park <sj@kernel.org>

--- src/damo_report_attrs.py
+++ src/damo_report_attrs.py
@@ -1549,7 +1549,7 @@ def set_formats_snapshot_default(fmt, records, args, ops_filters_installed):
 
 def set_formats_region_default(fmt, records, args):
     default_region_format = \
-            '<index> <start address> <size>  <access hz> <age> <probe hits> '
+            '<index> <start address> <size>  <age> <probe hits> '
     if args.region_box:
         if fmt.region_box_min_max_height[1] > 1:
             fmt.format_region = '<box>%s' % default_region_format
