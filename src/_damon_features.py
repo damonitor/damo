844f1bff7f153bbfe0a71228d66014ad6aaae90e
SJ Park <sj@kernel.org>
2026-07-19 12:18:24 -0700

    _damon_features: add sysfs/probe_type_pgidle_unset feature
    Signed-off-by: SJ Park <sj@kernel.org>

--- src/_damon_features.py
+++ src/_damon_features.py
@@ -368,6 +368,9 @@ features_list = [
         DamonFeature(name='sysfs/probe_prep_set_pgidle',
                      upstream_status='hacking on damon/next',
                      upstreamed_version='none'),
+        DamonFeature(name='sysfs/probe_type_pgidle_unset',
+                     upstream_status='hacking on damon/next',
+                     upstreamed_version='none'),
 
         DamonFeature(name='sysfs/damon_sample_control',
                      upstream_status='hacking on damon/next',
