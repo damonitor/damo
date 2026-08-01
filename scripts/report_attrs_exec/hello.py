02088d5c27b3ca0f45bc3238d10d8ec83cef95af
SJ Park <sj@kernel.org>
2026-08-01 12:05:58 -0700

    scripts: add sample script for 'damo report attrs --exec'
    Signed-off-by: SJ Park <sj@kernel.org>

---
+++ scripts/report_attrs_exec/hello.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0
+
+def main(damo_records, cmdline_fields):
+    print('Hello, you are executing this script with')
+    print('%d DamoRecords objects' % len(damo_records))
