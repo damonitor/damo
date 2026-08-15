8652f4b4ddc270f68bd9f678fbfad4ce37762943
SJ Park <sj@kernel.org>
2026-08-15 14:06:42 -0700

    damo_report_attrs: pass probe_weights to sz_hist_str()
    Signed-off-by: SJ Park <sj@kernel.org>

--- src/damo_report_attrs.py
+++ src/damo_report_attrs.py
@@ -471,8 +471,8 @@ def get_sz_region(region, fmt):
 def get_df_passed_sz_region(region, fmt):
     return region.sz_filter_passed
 
-def sz_hist_str(snapshot, fmt, df_passed_sz, get_metric_fn, aggr_us,
-                fmt_metric_fn, parse_metric_fn):
+def sz_hist_str(snapshot, fmt, df_passed_sz, get_metric_fn, probe_weights,
+                aggr_us, fmt_metric_fn, parse_metric_fn):
     if len(snapshot.regions) == 0:
         return 'no region in snapshot'
     if df_passed_sz is True:
@@ -495,7 +495,7 @@ def get_temperature(region, fmt, aggr_us):
 def temperature_sz_hist_str(
         snapshot, record, probe_weights, fmt, df_passed_sz):
     return sz_hist_str(
-            snapshot, fmt, df_passed_sz, get_temperature,
+            snapshot, fmt, df_passed_sz, get_temperature, probe_weights,
             infer_aggr_time_us(snapshot, record) , _damo_fmt_str.format_nr,
             _damo_fmt_str.text_to_nr)
 
@@ -510,7 +510,7 @@ def recency_hist_str(snapshot, record, probe_weights, fmt, df_passed_sz):
         return 'no region in snapshot'
 
     return sz_hist_str(
-            snapshot, fmt, df_passed_sz, get_idle_time,
+            snapshot, fmt, df_passed_sz, get_idle_time, probe_weights,
             infer_aggr_time_us(snapshot, record), _damo_fmt_str.format_time_us,
             _damo_fmt_str.text_to_us)
 
