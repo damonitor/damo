8db30d9425a1e307e91c594cec975c653a182910
SJ Park <sj@kernel.org>
2026-08-22 10:59:02 -0700

    damo_report_attrs: implement --region_sz
    Signed-off-by: SJ Park <sj@kernel.org>

--- src/damo_report_attrs.py
+++ src/damo_report_attrs.py
@@ -1391,6 +1391,53 @@ def validate_regions_sort_option(option):
             return 'unsupported input (%s)' % field
     return None
 
+def max_probe_hits_of(damon_record, kdamonds):
+    damon_ctx = kdamonds[
+            damon_record.kdamond_idx].contexts[damon_record.context_idx]
+    intervals = damon_ctx.intervals.aggr
+    return intervals.aggr / intervals.sample
+
+def adjust_region_sz_one(region, adjust_rules, max_probe_hits):
+    for rule_set in adjust_rules:
+        idx = 0
+        while idx < len(rule_set):
+            field = rule_set[idx]
+            idx += 1
+            if field == 'probe_hits_rate':
+                if idx == len(rule_set) - 1:
+                    return 'no probe idx'
+                idx += 1
+                probe_idx = rule_set[idx + 1]
+                if not probe_idx.isnumeric():
+                    return 'probe idx is not number'
+                probe_idx = int(probe_idx)
+                if probe_idx >= len(region.probe_hits):
+                    return 'probe idx >= %d' % len(region.probe_hits)
+                hit_rate = region.probe_hits[probe_idx] / max_probe_hits
+                new_sz = region.size() * hit_rate
+            elif field == 'df_passed':
+                if region.sz_filter_passed is None:
+                    return 'No DAMOS filter?'
+                new_sz = region.sz_filter_passed
+            region.end = region.start = new_sz
+    return None
+
+def adjust_region_sz(damo_records, adjust_rules):
+    '''Return error'''
+    if adjust_rules is None:
+        return None
+    for damo_record in damo_records:
+        kdamonds = damo_record.damon_records.kdamonds
+        for damon_record in damo_record.damon_records.record_list:
+            max_probe_hits = max_probe_hits_of(damon_record, kdamonds)
+            for snapshot in damon_record.snapshots:
+                for region in snapshot.regions:
+                    err = adjust_region_sz_one(
+                            region, adjust_rules, max_probe_hits)
+                    if err is not None:
+                        return err
+    return None
+
 def read_and_show(args):
     input_files, tried_regions_of_list, err = parse_input_option(args.input)
     if err is not None:
@@ -1460,6 +1507,12 @@ def read_and_show(args):
                 break
         if zero_damon_records and len(input_files) > 0 and repeat_count == 1:
             print('No record in %s' % input_files)
+
+        if args.region_sz:
+            err = adjust_region_sz(damo_records, args.region_sz)
+            if err is not None:
+                print('--region_sz fail (%s)' % err)
+                exit(1)
 
         if args.exec:
             err = handle_exec(args.exec, damo_records)
