e8dc85a5198d7aeaab5bbbd7a2304510e0f47c35
SJ Park <sj@kernel.org>
2026-09-13 14:01:33 -0700

    scripts/weighted_interleave: use --nr_* alternatives
    Signed-off-by: SJ Park <sj@kernel.org>

--- scripts/weighted_interleave.sh
+++ scripts/weighted_interleave.sh
@@ -31,8 +31,10 @@ then
 fi
 
 "$damo_bin" start \
-	--target_pid $target_proc --ops vaddr \
-		--monitoring_intervals_goal 4% 3 5ms 10s \
-		--damos_action migrate_hot 0 1 1 1 --damos_access_rate 5% max \
-		--damos_apply_interval 1s \
-		--damos_quota_interval 1s --damos_quota_space 200MB \
+	--kdamond --damon_ctx --ops vaddr \
+			--monitoring_intervals_goal 4% 3 5ms 10s \
+		--damon_target --target_pid $target_proc \
+		--damos_scheme \
+			--damos_action migrate_hot 0 1 1 1 \
+			--damos_access_rate 5% max --damos_apply_interval 1s \
+			--damos_quota_interval 1s --damos_quota_space 200MB \
